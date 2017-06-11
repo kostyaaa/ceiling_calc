d67b5bd1710499d91bfe9b7a9ff2fdb9a42aa501
kostyaaa <k.kostya.ne@gmail.com>
2017-06-11 10:37:45 +0500

    form1 rename to "ceil_calc"

--- ceilings/Form1.cs
+++ ceilings/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ceilings
 {
-    public partial class Form1 : Form
+    public partial class Ceil_Calc : Form
     {
-        public Form1()
+        public Ceil_Calc()
         {
             InitializeComponent();
         }
