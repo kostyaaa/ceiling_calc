1342a230cbaa8e0019519d2b02b926561e3fea1d
kostyaaa <k.kostya.ne@gmail.com>
2017-06-11 20:11:36 +0500

    мелкие изменения

--- ceilings/Form1.Designer.cs
+++ ceilings/Form1.Designer.cs
@@ -55,6 +55,10 @@
             this.label1_2 = new System.Windows.Forms.Label();
             this.label0_6 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tpSquareCount.SuspendLayout();
@@ -118,6 +122,10 @@
             // 
             // tpSquareCount
             // 
+            this.tpSquareCount.Controls.Add(this.label8);
+            this.tpSquareCount.Controls.Add(this.label7);
+            this.tpSquareCount.Controls.Add(this.label6);
+            this.tpSquareCount.Controls.Add(this.label5);
             this.tpSquareCount.Controls.Add(this.label9);
             this.tpSquareCount.Controls.Add(this.label0_6);
             this.tpSquareCount.Controls.Add(this.label1_2);
@@ -236,7 +244,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 136);
+            this.label1.Location = new System.Drawing.Point(37, 146);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 12;
@@ -245,7 +253,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 153);
+            this.label2.Location = new System.Drawing.Point(37, 176);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 13);
             this.label2.TabIndex = 13;
@@ -255,7 +263,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 170);
+            this.label3.Location = new System.Drawing.Point(37, 206);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(22, 13);
             this.label3.TabIndex = 14;
@@ -264,7 +272,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 187);
+            this.label4.Location = new System.Drawing.Point(37, 236);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(22, 13);
             this.label4.TabIndex = 15;
@@ -273,7 +281,7 @@
             // labelPlita
             // 
             this.labelPlita.AutoSize = true;
-            this.labelPlita.Location = new System.Drawing.Point(146, 136);
+            this.labelPlita.Location = new System.Drawing.Point(201, 146);
             this.labelPlita.Name = "labelPlita";
             this.labelPlita.Size = new System.Drawing.Size(13, 13);
             this.labelPlita.TabIndex = 16;
@@ -282,7 +290,7 @@
             // label3_7
             // 
             this.label3_7.AutoSize = true;
-            this.label3_7.Location = new System.Drawing.Point(146, 153);
+            this.label3_7.Location = new System.Drawing.Point(201, 176);
             this.label3_7.Name = "label3_7";
             this.label3_7.Size = new System.Drawing.Size(13, 13);
             this.label3_7.TabIndex = 17;
@@ -291,7 +299,7 @@
             // label1_2
             // 
             this.label1_2.AutoSize = true;
-            this.label1_2.Location = new System.Drawing.Point(146, 170);
+            this.label1_2.Location = new System.Drawing.Point(201, 206);
             this.label1_2.Name = "label1_2";
             this.label1_2.Size = new System.Drawing.Size(13, 13);
             this.label1_2.TabIndex = 18;
@@ -300,7 +308,7 @@
             // label0_6
             // 
             this.label0_6.AutoSize = true;
-            this.label0_6.Location = new System.Drawing.Point(146, 183);
+            this.label0_6.Location = new System.Drawing.Point(201, 245);
             this.label0_6.Name = "label0_6";
             this.label0_6.Size = new System.Drawing.Size(13, 13);
             this.label0_6.TabIndex = 19;
@@ -314,6 +322,40 @@
             this.label9.Size = new System.Drawing.Size(201, 13);
             this.label9.TabIndex = 20;
             this.label9.Text = "Необходимое количество материалов";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Уголок";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Европодвес";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(201, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 24;
             // 
             // Ceil_Calc
             // 
@@ -363,6 +405,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
 
